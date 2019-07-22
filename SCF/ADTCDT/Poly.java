b97387dbc230bb0124ea4298cf5a522e01f2310f
vinod kumar sahu <vinod.sahu@metacube.com>
2019-07-22 18:39:36 +0530

    submittes assignment 6 based on abstract and concrete data type

--- SCF/ADTCDT/Poly.java
+++ SCF/ADTCDT/Poly.java
@@ -1,18 +1,15 @@
 /**
-* Implement the immutable class Poly using an array to represent single variable polynomial. Note that you should be storing only those terms of polynomial that have non zero coefficient. You can assume that the coefficients are integers
-
-* It should support methods like - 
-
-* evaluate(float) - return the value of the polynomial for the given value of the variable
-
-* degree() - return the degree of the polynomial
-
-* addPoly(Poly p1, Poly p2) - return the sum of the polynomials p1 and p2
-
-* multiplyPoly(Poly p1, Poly p2) - return the product of polynomials p1 and p2
-
-* You may use private helper methods. addPoly() and multiplyPoly() can make assumptions about size of the resulting polynomial to decide about the size of the array to be created.
-*/
+ * Implement the immutable class Poly using an array to represent single variable polynomial. Note that you should be storing only those terms of
+ * polynomial that have non zero coefficient. You can assume that the coefficients are integers
+ * It should support methods like -
+ * evaluate(float) - return the value of the polynomial for the given value of
+ * the variable degree() - return the degree of the polynomial
+ * addPoly(Poly p1, Poly p2) - return the sum of the polynomials p1 and p2
+ * multiplyPoly(Poly p1, Poly p2) - return the product of polynomials p1 and p2
+ * You may use private helper methods. addPoly() and multiplyPoly() can make
+ * assumptions about size of the resulting polynomial to decide about the size
+ * of the array to be created.
+ */
 
 public final class Poly {
 	private final int polynomial[][];
@@ -22,19 +19,19 @@ public final class Poly {
 	 */
 	public Poly(int poly[]) throws Exception {
 
-		if(poly.length==0)
+		if (poly.length == 0)
 			throw new Exception("Empty Polynomial Array");
 		int i, count = 0;
-		for( i = 0; i < poly.length ; i++ )
-			if(poly[i]!=0){
+		for (i = 0; i < poly.length; i++)
+			if (poly[i] != 0) {
 				count++;
 			}
 		polynomial = new int[count][2];
-		count=0;
-		for( i = 0; i < poly.length ; i++ ){
-			if(poly[i]!=0){
-				polynomial[count][0]=i;
-				polynomial[count][1]=poly[i];
+		count = 0;
+		for (i = 0; i < poly.length; i++) {
+			if (poly[i] != 0) {
+				polynomial[count][0] = i;
+				polynomial[count][1] = poly[i];
 				count++;
 			}
 		}
@@ -42,33 +39,32 @@ public final class Poly {
 
 	/**
 	 * @param valueOfX is an integer value that is used as value of x in the polynomial evaluation
-	 * @return returns the value of polynomial equation after evaluation 
+	 * @return returns the value of polynomial equation after evaluation
 	 */
-	float evaluate(float valueOfX){
-		float value=0;
-		for(int i=0; i < polynomial.length ;i++)
-		value += java.lang.Math.pow(valueOfX,polynomial[i][0]) * polynomial[i][1];
+	float evaluate(float valueOfX) {
+		float value = 0;
+		for (int i = 0; i < polynomial.length; i++)
+			value += java.lang.Math.pow(valueOfX, polynomial[i][0])
+					* polynomial[i][1];
 		return value;
 	}
 
 	/**
 	 * @returns return the maximum degree of the polynomial equation
 	 */
-	int degree()
-	{
-		int degreeOfPolynomial=polynomial[polynomial.length-1][0];		
+	int degree() {
+		int degreeOfPolynomial = polynomial[polynomial.length - 1][0];
 		return degreeOfPolynomial;
 	}
 
 	/**
-	 * @param index is an integer value that is used to see if there exist any X^index in the equation
-	 * @returns return an integer value that indicate that the power of x exist in the equation
+	 * @param index is an integer value that is used to see if there existanyX^index in the equation
+	 * @returns return an integer value that indicate that the power of x existin the equation
 	 */
-	private int getIndexOf(int index)
-	{
+	private int getIndexOf(int index) {
 		int x = -1;
-		for(int i = 0; i < polynomial.length ; i++ )
-			if(polynomial[i][0]==index)
+		for (int i = 0; i < polynomial.length; i++)
+			if (polynomial[i][0] == index)
 				x = polynomial[i][0];
 		return x;
 	}
@@ -77,66 +73,69 @@ public final class Poly {
 	 * @param index is an integer value that is used to see if there exist any X^index in the equation
 	 * @returns return an integer value that is the coefficient value of x at x^index in the equation
 	 */
-	private int getValueOf(int index)
-	{
+	private int getValueOf(int index) {
 		int x = -1;
-		for(int i = 0; i < polynomial.length ; i++ )
-			if(polynomial[i][0]==index)
+		for (int i = 0; i < polynomial.length; i++)
+			if (polynomial[i][0] == index)
 				x = polynomial[i][1];
 		return x;
 	}
+
 	/**
-	 * @param p1 and @param p2 are the two objects of Poly class representing two polynomial equation
-	 * @returns return the addition of the two Poly objects 
+	 * @param p1and 
+	 * @param p2 are the two objects of Poly class representing two polynomial equation
+	 * @returns return the addition of the two Poly objects
 	 */
-	int[] addPoly(Poly p1, Poly p2){ 
-		int i, maxDegree = p1.degree() > p2.degree() ? p1.degree() + 1 : p2.degree() + 1  ;
-		int sum[] = new int[ maxDegree] ;
-		for(i = 0; i < maxDegree ; i++){
-			if(p1.getIndexOf(i) == p2.getIndexOf(i))
+	int[] addPoly(Poly p1, Poly p2) {
+		int i, maxDegree = p1.degree() > p2.degree() ? p1.degree() + 1 : p2
+				.degree() + 1;
+		int sum[] = new int[maxDegree];
+		for (i = 0; i < maxDegree; i++) {
+			if (p1.getIndexOf(i) == p2.getIndexOf(i))
 				sum[i] = p1.getValueOf(i) + p2.getValueOf(i);
-			else if(-1 == p2.getIndexOf(i))
+			else if (-1 == p2.getIndexOf(i))
 				sum[i] = p1.getValueOf(i);
-			else if(-1 == p1.getIndexOf(i))
+			else if (-1 == p1.getIndexOf(i))
 				sum[i] = p2.getValueOf(i);
 			else
 				sum[i] = 0;
-		}		
+		}
 		return (sum);
 	}
 
 	/**
-	 * @param p1 and @param p2 are the two objects of Poly class representing two polynomial equation
-	 * @returns return the Multiplication of the two Poly objects 
+	 * @param p1 
+	 * @param p2 are the two objects of Poly class representing two polynomial equation
+	 * @returns return the Multiplication of the two Poly objects
 	 */
-	int[] multiplyPoly(Poly p1, Poly p2){ 
-		int i, j, maxDegree = (p1.degree() * p2.degree())+1 ;
-		int multiply[] = new int[ maxDegree] ;
-		for(i = 0; i <= p1.degree() ; i++){
-			for(j = 0; j <= p2.degree() ; j++){
+	int[] multiplyPoly(Poly p1, Poly p2) {
+		int i, j, maxDegree = (p1.degree() * p2.degree()) + 1;
+		int multiply[] = new int[maxDegree];
+		for (i = 0; i <= p1.degree(); i++) {
+			for (j = 0; j <= p2.degree(); j++) {
 				{
-					if(p1.getIndexOf(i) > -1 && p2.getIndexOf(j) > -1)
+					if (p1.getIndexOf(i) > -1 && p2.getIndexOf(j) > -1)
 
-					multiply[p1.getIndexOf(i) + p2.getIndexOf(j)] += p1.getValueOf(i) * p2.getValueOf(j);
-				}	
+						multiply[p1.getIndexOf(i) + p2.getIndexOf(j)] += p1
+								.getValueOf(i) * p2.getValueOf(j);
+				}
 			}
-		}		
+		}
 		return (multiply);
 	}
 
 	/**
-	 * prints the polynomial equation 
+	 * prints the polynomial equation
 	 */
-	void print()
-	{
+	void print() {
 		int i = 0;
-		for(i = polynomial.length-1 ; i >= 0 ; i--){
-			if(polynomial[i][0] == 0)
+		for (i = polynomial.length - 1; i >= 0; i--) {
+			if (polynomial[i][0] == 0)
 				System.out.print(polynomial[i][1] + " ");
 			else
 				System.out.print(polynomial[i][1] + "X^" + polynomial[i][0]);
 
-			if(i == 0)
+			if (i == 0)
 				continue;
 			else
 				System.out.print(" + ");
--- SCF/ADTCDT/PolyTest.java
+++ SCF/ADTCDT/PolyTest.java
@@ -5,38 +5,41 @@ import org.junit.Test;
 
 public class PolyTest {
 
-	// test to check Empty Array
 	@Test
 	public void test_Empty_Array() {
-		   try { new Poly(new int[]{ }); }
-		   catch (Exception e) { assertEquals("Empty Polynomial Array", e.getMessage());}
-	   }
-
-	// positive test case for evaluation of Polynomial
-	@Test
-	 public void test_Evaluate_Positive() throws Exception {
-		Assert.assertEquals(73.00,new Poly(new int[]{1,0,2,0,4}).evaluate(2),0.0f);
-	   }
-
-	// test case for finding degree of polynomial
-	@Test
-	 public void test_Degree_Positive() throws Exception {
-		 assertEquals(3,new Poly(new int[]{0,1,0,5}).degree());
-	   }
-
-	// positive test case for addition of two polynomial
-	@Test
-	   public void test_Add_Polyinomial() throws Exception{	
-		   int returnedArrOperation[] = new Poly(new int[]{1,0,2,0,4}).addPoly(new Poly(new int[]{1,0,2,0,4}),new Poly(new int[]{0,1,0,5}));
-		   int []expectedArr = new int[]{1,1,2,5,4};
-		   Assert.assertArrayEquals( expectedArr, returnedArrOperation );
-	   }
-
-	// positive test case for multiplication of two polynomial
-	@Test
-	   public void test_Multiply_Polyinomial() throws Exception{	
-		   int returnedArrOperation[] = new Poly(new int[]{1,0,2,0,4}).multiplyPoly(new Poly(new int[]{1,0,2,0,4}),new Poly(new int[]{0,1,0,5}));
-		   int []expectedArr = new int[]{0,1,0,7,0,14,0,20,0,0,0,0,0};
-		   Assert.assertArrayEquals( expectedArr, returnedArrOperation );
-	   }
+		try {
+			new Poly(new int[] {});
+		} catch (Exception e) {
+			assertEquals("Empty Polynomial Array", e.getMessage());
+		}
 	}
+
+	@Test
+	public void test_Evaluate_Positive() throws Exception {
+		Assert.assertEquals(73.00,
+				new Poly(new int[] { 1, 0, 2, 0, 4 }).evaluate(2), 0.0f);
+	}
+
+	@Test
+	public void test_Degree_Positive() throws Exception {
+		assertEquals(3, new Poly(new int[] { 0, 1, 0, 5 }).degree());
+	}
+
+	@Test
+	public void test_Add_Polyinomial() throws Exception {
+		int returnedArrOperation[] = new Poly(new int[] { 1, 0, 2, 0, 4 })
+				.addPoly(new Poly(new int[] { 1, 0, 2, 0, 4 }), new Poly(
+						new int[] { 0, 1, 0, 5 }));
+		int[] expectedArr = new int[] { 1, 1, 2, 5, 4 };
+		Assert.assertArrayEquals(expectedArr, returnedArrOperation);
+	}
+
+	@Test
+	public void test_Multiply_Polyinomial() throws Exception {
+		int returnedArrOperation[] = new Poly(new int[] { 1, 0, 2, 0, 4 })
+				.multiplyPoly(new Poly(new int[] { 1, 0, 2, 0, 4 }), new Poly(
+						new int[] { 0, 1, 0, 5 }));
+		int[] expectedArr = new int[] { 0, 1, 0, 7, 0, 14, 0, 20, 0, 0, 0, 0, 0 };
+		Assert.assertArrayEquals(expectedArr, returnedArrOperation);
+	}
+}
