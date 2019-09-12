ca82021a9f7354d56ee90017e37e41be81cf0a63
vinod kumar sahu <vinod.sahu@metacube.com>
2019-09-12 17:08:35 +0530

    submit assignment8 of EAD

---
+++ EAD/ASSIGNMENT8/Student/src/main/java/com/metacube/Student/utils/ListStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Contains Hard-Coded List of Students
+ */
+package com.metacube.Student.utils;
+
+import com.metacube.Student.model.Student;
+
+public class ListStudent {
+	
+	public static final Student s1 = new Student("vinod","sahu","rakesh","vinod.sahu@gmail.com","1","21");
+	public static final Student s2 = new Student("vinod2","sahu2","naman","vin.sah@gmail.com","1","21");
+	public static final Student s3 = new Student("vinod3","sahu3","jyoti","vinod@gmail.com","1","21");
+	public static final Student s4 = new Student("vinod4","sahu4","abhi","vinod4@gmail.com","1","21");
+	public static final Student s5 = new Student("naman","vijay","viashali","naman@gmail.com","1","21");
+}
