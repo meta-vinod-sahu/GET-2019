8e3434b2de63e1d87f19e14c487b8b2894ea1fbe
vinod kumar sahu <vinod.sahu@metacube.com>
2019-09-23 18:43:06 +0530

    submit assignment of EAD 14

---
+++ EAD/ASSIGNMENT14/SpringSecurityJDBC/src/test/java/com/metacube/SpringSecurityJDBC/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,19 @@
+package com.metacube.SpringSecurityJDBC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * 
+ * @author Pankaj
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
