541e858cb962a32d1a172674845e7951487bd52e
meta-vinod-sahu <52955962+meta-vinod-sahu@users.noreply.github.com>
2019-09-27 13:49:21 +0530

    Update EadSession13AssignmentApplication.java

--- EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/EadSession13AssignmentApplication.java
+++ EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/EadSession13AssignmentApplication.java
@@ -3,11 +3,7 @@ package com.metacube.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-/**
- * 
- * @author Pankaj
- *
- */
+
 @SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
 public class EadSession13AssignmentApplication {
 
