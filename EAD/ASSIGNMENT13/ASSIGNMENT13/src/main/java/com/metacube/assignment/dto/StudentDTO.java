713dc0c47b7ebffd54102778e5db01f257a22a34
meta-vinod-sahu <52955962+meta-vinod-sahu@users.noreply.github.com>
2019-09-27 13:52:55 +0530

    Update StudentDTO.java

--- EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/dto/StudentDTO.java
+++ EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/dto/StudentDTO.java
@@ -5,11 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-/**
- * 
- * @author Pankaj
- *
- */
+
 public class StudentDTO {
 	
 	
