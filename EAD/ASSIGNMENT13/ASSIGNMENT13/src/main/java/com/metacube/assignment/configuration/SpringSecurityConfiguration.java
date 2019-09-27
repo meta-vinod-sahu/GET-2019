7940c73f2941ee590871d0908a7a5bd172fac204
meta-vinod-sahu <52955962+meta-vinod-sahu@users.noreply.github.com>
2019-09-27 13:50:00 +0530

    Update SpringSecurityConfiguration.java

--- EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/configuration/SpringSecurityConfiguration.java
+++ EAD/ASSIGNMENT13/ASSIGNMENT13/src/main/java/com/metacube/assignment/configuration/SpringSecurityConfiguration.java
@@ -7,11 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-/**
- * 
- * @author Pankaj
- *
- */
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
